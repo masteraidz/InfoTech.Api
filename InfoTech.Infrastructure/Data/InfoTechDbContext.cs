7183e8890a397e1898c548d9bca323222a55e3fc
Adrian <piuaidz04@gmail.com>
2025-01-02 10:32:01 +0800

    Update InfoTechDbContext.cs to Use Schema

--- InfoTech.Infrastructure/Data/InfoTechDbContext.cs
+++ InfoTech.Infrastructure/Data/InfoTechDbContext.cs
@@ -1,4 +1,4 @@
-﻿using InfoTech.Core.Entities;
+using InfoTech.Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace InfoTech.Infrastructure.Data
@@ -6,5 +6,11 @@ namespace InfoTech.Infrastructure.Data
     public class InfoTechDbContext(DbContextOptions<InfoTechDbContext> options) : DbContext(options)
     {
         public DbSet<UserEntity> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserEntity>()
+                .ToTable("Users", "Tech");
+        }
     }
 }
